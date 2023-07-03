107afcb4038dd6f89251193390c0750e62386c6d
Nishant Jha <nkj.nishant@gmail.com>
2023-07-03 15:02:49 +0530

    Comments in Graph added

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,3 +1,6 @@
+// For creating graph, a libarary called Recharts is used, which is a React based library for
+// generating graphs according to the data it is provided.
+
 import React, { useContext, useEffect, useState } from "react";
 import ChartFilter from "./ChartFilter";
 import Card from "./Card";
@@ -15,19 +18,19 @@ import { fetchHistoricalData } from "../utils/api/stock-api";
 import { chartConfig } from "../constants/config";
 
 const Chart = () => {
-  const [filter, setFilter] = useState("1W");
+  const [filter, setFilter] = useState("1W"); // Initializing the filter with "1 Week", so that initially the graph shows past 7 days data
 
-  const { darkMode } = useContext(ThemeContext);
+  const { darkMode } = useContext(ThemeContext); // Context state which holds the value of current applied theme (light or dark)
 
-  const { stockSymbol } = useContext(StockContext);
+  const { stockSymbol } = useContext(StockContext); // Context state which holds the value of selected stock's symbol
 
   const [data, setData] = useState([]);
 
-  const formatData = (data) => {
+  const formatData = (data) => { // Function to format the data according to the input which Areachart expects
     let graphData = [];
-    let sliceNumber = filter === "1W" ? 7 : filter === "1M" ? 30 : 12;
+    let sliceNumber = filter === "1W" ? 7 : filter === "1M" ? 30 : 12; // Since there is huge amount of data from the API, only the required amount of data is being sliced and provided to the graph
     if(data) {
-      graphData = Object.keys(data).slice(0, sliceNumber).map((item, index) => {
+      graphData = Object.keys(data).slice(0, sliceNumber).map((item, index) => { // Iterate over object's keys and return an array of objects in this - [{"value": 229, date: "22-05-2023"}] form
         return {
           value: data[item]["4. close"],
           date: item,
@@ -53,14 +56,14 @@ const Chart = () => {
     };
 
     updateChartData();
-  }, [stockSymbol, filter]);
+  }, [stockSymbol, filter]); // Stocksymbol and filter are provided as parameters whose change in value, triggers the change in graph
 
   return (
     <Card>
       <ul className="flex absolute top-2 right-2 z-40">
         {chartConfig.map((item) => (
           <li key={item}>
-            <ChartFilter
+            <ChartFilter // Component for rendering the chart filters, 1W, 1M or 1Y
               text={item}
               active={filter === item}
               onClick={() => {
@@ -86,7 +89,7 @@ const Chart = () => {
               />
             </linearGradient>
           </defs>
-          <Tooltip
+          <Tooltip // Built in component of AreaChart that shows tooltip on mouse-hover
             contentStyle={darkMode ? { backgroundColor: "#111827" } : null}
             itemStyle={darkMode ? { color: "#818cf8" } : null}
           />
@@ -98,8 +101,8 @@ const Chart = () => {
             fillOpacity={1}
             strokeWidth={0.5}
           />
-          <XAxis dataKey="date"/>
-          <YAxis domain={[0, dataMax => (Math.ceil(dataMax))]} type="number" tickCount={7} dataKey="value" />
+          <XAxis dataKey="date"/> {/* "Date" is supplied to X axis' data key */}
+          <YAxis domain={[0, dataMax => (Math.ceil(dataMax))]} type="number" tickCount={7} dataKey="value" /> {/* The domain of Y axis contains points from 0 to the ceil round-off (whole number) of the maximum data */}
         </AreaChart>
       </ResponsiveContainer>
     </Card>
